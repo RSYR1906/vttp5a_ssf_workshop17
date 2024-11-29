c07fb0ca46a821a359d7bd27c44d0fab6da8bc81
RSYR1906 <rsim9977@gmail.com>
2024-11-29 17:51:27 +0800

    first commit

---
+++ src/test/java/com/sg/nus/iss/Workshop_17/Workshop17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.nus.iss.Workshop_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
